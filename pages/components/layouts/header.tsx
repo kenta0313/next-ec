2d0afa7a8f70e8a7749e17f74e1bd608317adc3d
kenta0313 <asanokenta@me.com>
2021-09-03 15:21:14 +0900

    🎨Header作成

---
+++ pages/components/layouts/header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className="w-full h-12 bg-gray-200">
+            <div className="text-center py-2">
+                Next-EC
+            </div>
+        </header>
+    )
+}
+
+export default Header;
